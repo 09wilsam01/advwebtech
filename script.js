f927233abb9b0a57963d83f4e30dd76479773109
Sam W-P <145544456+09wilsam01@users.noreply.github.com>
2024-11-15 13:53:44 +0000

    UI work done on login.html

--- script.js
+++
@@ -1,7 +0,0 @@
-function enter(){
-	localStorage.clear();
-	let username = document.getElementById("user").value;
-	localStorage.setItem('setUser', username);
-	let password = document.getElementById("pass").value;
-	localStorage.setItem('setPass', password);
-}
