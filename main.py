eb745c24006524f57f8528d240dd4fe0d487e535
Sam W-P <145544456+09wilsam01@users.noreply.github.com>
2024-11-18 16:20:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, abort
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return index.html
-    
+   return render_template('index.html')
+
+@app.route('/force404')
+def force404():
+    abort(404)
+
+@app.errorhandler(404)
+def  page_not_found(error):
+    return "Couldn't find the page you requested", 404
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug = True)
