f0231cee1af66ae26c1f0e76c10514952856f332
Sam W-P <145544456+09wilsam01@users.noreply.github.com>
2024-11-25 14:20:03 +0000

    app upgraded

--- app.py
+++ app.py
@@ -42,13 +42,5 @@ def home():
 def passreset():
     return render_template('passreset.html')
 
-@app.route('/force404')
-def force404():
-    abort(404)
-
-@app.errorhandler(404)
-def  page_not_found(error):
-    return "Couldn't find the page you requested", 404
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug = True)
