49046e93f3f4415889e3cf5049428d8681bfe1d3
Sam W-P <145544456+09wilsam01@users.noreply.github.com>
2024-11-25 14:49:52 +0000

    app

--- app.py
+++ app.py
@@ -30,17 +30,5 @@ def index():
 def login():
     return render_template('login.html')
 
-@app.route("/signup")
-def signup():
-    return render_template('signup.html')
-
-@app.route("/home")
-def home():
-    return render_template('home.html')
-
-@app.route("/passreset")
-def passreset():
-    return render_template('passreset.html')
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug = True)
