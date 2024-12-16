cc9a0c2d8ce5c7308cea638dc9f9c71d71dbdb09
Sam W-P <145544456+09wilsam01@users.noreply.github.com>
2024-12-16 21:16:08 +0000

    app update

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
 from flask import Flask, render_template
 app = Flask ( __name__ )
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route("/home")
+@app.route('/home')
 def home():
     return render_template('home.html')
 
-@app.route("/19-22")
+@app.route('/19-22')
 def home():
     return render_template('19-22.html')
 
-@app.route("/23")
+@app.route('/23')
 def home():
     return render_template('23.html')
 
-@app.route("/24")
+@app.route('/24')
 def home():
     return render_template('24.html')
 
-@app.route("/aboutme")
+@app.route('/aboutme')
 def home():
     return render_template('aboutme.html')
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug = True)
