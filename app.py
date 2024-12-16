1c4d9dadb56bc8db94a900aa8f347bd033f20a77
Sam W-P <145544456+09wilsam01@users.noreply.github.com>
2024-12-16 20:49:51 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -9,5 +9,21 @@ def index():
 def home():
     return render_template('home.html')
 
+@app.route("/19-22")
+def home():
+    return render_template('19-22.html')
+
+@app.route("/23")
+def home():
+    return render_template('23.html')
+
+@app.route("/24")
+def home():
+    return render_template('24.html')
+
+@app.route("/aboutme")
+def home():
+    return render_template('aboutme.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug = True)
